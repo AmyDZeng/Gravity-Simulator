90f89725a89e9dd46f1a28c1af93b26fe9611d01
Gary Lin <gary.96.lin@gmail.com>
2015-10-04 11:06:02 -0400

    Added functions in simulator.java for changing instructions, and modified id of TextView in simulation.xml`

--- Gravity-Simulator/app/src/main/java/com/terriblehacks/gravitysimulator/Simulator.java
+++ Gravity-Simulator/app/src/main/java/com/terriblehacks/gravitysimulator/Simulator.java
@@ -3,11 +3,13 @@ package com.terriblehacks.gravitysimulator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
 
 /**
  * Created by Malzberry on 10/4/2015.
  */
 public class Simulator extends Activity {
+    TextView instructions;
     protected void onCreate(Bundle savedInstanceState){
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
@@ -24,4 +26,15 @@ public class Simulator extends Activity {
     protected void onPause() {
         super.onPause();
     }
+
+    public void onLift(){
+        instructions = (TextView) findViewById(R.id.instructions);
+        instructions.setText("Let Go.");
+    }
+
+    public void onDrop(){
+        instructions = (TextView) findViewById(R.id.instructions);
+        instructions.setText("Gravity Simulation Complete.");
+    }
+
 }
