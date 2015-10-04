337777d6168b9810860bc993a52e6bc91ba7c49a
AmyDZeng <amy.d.zeng@gmail.com>
2015-10-04 11:11:13 -0400

    HUE

--- Gravity-Simulator/app/src/main/java/com/terriblehacks/gravitysimulator/Simulator.java
+++ Gravity-Simulator/app/src/main/java/com/terriblehacks/gravitysimulator/Simulator.java
@@ -1,27 +1,66 @@
 package com.terriblehacks.gravitysimulator;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Window;
 
 /**
  * Created by Malzberry on 10/4/2015.
  */
-public class Simulator extends Activity {
+public class Simulator extends Activity implements SensorEventListener{
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private float y;
+    private long lastUpdate;
+    //private static final int FALL_THRESHOLD;
+    //private static final int LIFT_THRESHOLD;
     protected void onCreate(Bundle savedInstanceState){
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.simulator);
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+
+        lastUpdate = System.currentTimeMillis();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mySensor = event.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            float y = event.values[0];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
