7865e4842ab2004d93db87bac1e19448a2af44c7
AmyDZeng <amy.d.zeng@gmail.com>
2015-10-04 12:35:27 -0400

    ye

--- Gravity-Simulator/app/src/main/java/com/terriblehacks/gravitysimulator/Simulator.java
+++ Gravity-Simulator/app/src/main/java/com/terriblehacks/gravitysimulator/Simulator.java
@@ -16,12 +16,22 @@ import android.widget.TextView;
 public class Simulator extends Activity implements SensorEventListener{
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
-    private float y;
+    private float initialY = 0, lastY, last_x, last_y, last_z;
     private long lastUpdate;
+    Boolean lifted, dropped, start;
     //private static final int FALL_THRESHOLD;
     //private static final int LIFT_THRESHOLD;
 
     TextView instructions;
+    TextView testText;
+    TextView testTextMax;
+    TextView testTextMin;
+    float maxY = 0, minY = 0;
+
+    float gravity[] = {0, (float)-9.8, 0};
+    float linear_acceleration[] = {0,0,0};
+    float alpha = (float) 0.8;
+
     protected void onCreate(Bundle savedInstanceState){
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
@@ -32,6 +42,9 @@ public class Simulator extends Activity implements SensorEventListener{
         senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
 
         lastUpdate = System.currentTimeMillis();
+        lifted = false;
+        dropped = false;
+        start = false;
     }
 
     @Override
@@ -51,13 +64,62 @@ public class Simulator extends Activity implements SensorEventListener{
         Sensor mySensor = event.sensor;
 
         if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            float y = event.values[0];
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+
+
+
+            if (start == false){
+                initialY = y;
+                start = true;
+            }
+
+            last_x = x;
+            last_y = y;
+            last_z = z;
+
+
 
             long curTime = System.currentTimeMillis();
 
             if ((curTime - lastUpdate) > 100) {
                 long diffTime = (curTime - lastUpdate);
                 lastUpdate = curTime;
+                lastY = y;
+
+                if (lifted == false && dropped == false){
+
+                }
+                // TESTS
+
+
+
+                gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+                gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+                gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+                linear_acceleration[0] = event.values[0] - gravity[0];
+                linear_acceleration[1] = event.values[1] - gravity[1];
+                linear_acceleration[2] = event.values[2] - gravity[2];
+
+
+                //float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 1000000000;
+                testText = (TextView) findViewById(R.id.testText);
+                //double acc = calculateAcceleration(event.values);
+                testText.setText(Float.toString(linear_acceleration[1]));
+
+                if(linear_acceleration[1] > maxY)maxY = linear_acceleration[1];
+
+                testTextMax = (TextView) findViewById(R.id.testTextMax);
+                testTextMax.setText(Float.toString(maxY));
+
+                if(linear_acceleration[1] < minY)minY = linear_acceleration[1];
+
+                testTextMin = (TextView) findViewById(R.id.testTextMin);
+                testTextMin.setText(Float.toString(minY));
+
             }
         }
     }
@@ -67,9 +129,15 @@ public class Simulator extends Activity implements SensorEventListener{
 
     }
 
+    public static double calculateAcceleration(float[] values) {
+        double acceleration = Math.sqrt(Math.pow(values[0], 2)
+                + Math.pow(values[1], 2) + Math.pow(values[2], 2));
+        return acceleration;
+    }
+
     public void onLift(){
         instructions = (TextView) findViewById(R.id.instructions);
-        instructions.setText("Let Go.");
+        instructions.setText("Drop it like it's hot");
     }
 
     public void onDrop(){
